951a89f3ad1f2cbb13a03f1a71002feb13fd6e00
Rui Peres <sauron.is.alive@gmail.com>
2014-12-03 20:27:39 +0000

    Added +++ and reverse2

---
+++ chapter2/Chapter2/SimpleFunctions.hs
@@ -0,0 +1,12 @@
+firstOrEmpty :: [[Char]] -> [Char]
+firstOrEmpty lst = if not (null lst) then head lst else "empty"
+
+(+++) :: [a] -> [a] -> [a]
+lst1 +++ lst2 = if null lst1  {- check emptyness -}
+                then lst2      -- base case
+                else (head lst1) : (tail lst1 +++ lst2)
+
+reverse2 :: [a] -> [a]
+reverse2 list = if null list
+ 				then []
+                else reverse2 (tail list) +++ [head list]
