02f93c5b906a2f04e21313449d888a0f878c00f6
Rui Peres <sauron.is.alive@gmail.com>
2014-12-03 19:47:55 +0000

    Exercise 3

--- Exercise2_1.hs
+++ Exercise2_1.hs
@@ -5,9 +5,16 @@
 exercise1 :: [[Char]]
 exercise1 = ('a':'b':'c':[]) : ('d' : 'e' : []) : [[]]
 
---2. Write an expression that checks whether a list is empty, [], or its first element is empty, like [[],['a','b']].
+-- 2. Write an expression that checks whether a list is empty, [], or its first element is empty, like [[],['a','b']].
 
 exercise2 :: [[a]] -> Bool 
 exercise2 [] = True
 exercise2 ([]:xs) = True
 exercise2 _ = False
+
+-- 3. Write an expression that checks whether a list has only one element. It should return True for ['a'] and False for [] or ['a','b'].
+
+exercise3 :: [a] -> Bool 
+exercise3 [] = False
+exercise3 (_:[]) = True
+exercise3 x = False
