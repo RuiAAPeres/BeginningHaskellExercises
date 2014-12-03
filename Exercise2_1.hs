6859ee5514b2193dcedb447164604fdc3f440178
Rui Peres <sauron.is.alive@gmail.com>
2014-12-03 19:41:06 +0000

    Exercise 2

--- Exercise2_1.hs
+++ Exercise2_1.hs
@@ -4,3 +4,10 @@
 
 exercise1 :: [[Char]]
 exercise1 = ('a':'b':'c':[]) : ('d' : 'e' : []) : [[]]
+
+--2. Write an expression that checks whether a list is empty, [], or its first element is empty, like [[],['a','b']].
+
+exercise2 :: [[a]] -> Bool 
+exercise2 [] = True
+exercise2 ([]:xs) = True
+exercise2 _ = False
