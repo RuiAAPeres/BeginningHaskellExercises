b6971becf014abcf4b80c5088cdc908da0dba221
RPeres <rui.peres@williamhill.com>
2014-12-04 13:26:03 +0000

    Added function to read the name of a Client

--- chapter2/src/Chapter2/DataTypes.hs
+++ chapter2/src/Chapter2/DataTypes.hs
@@ -2,5 +2,21 @@ module Chapter2.DataTypes where
 
 data Client = GovOrg String
             | Company    String Integer String String
-			| Individual String String Bool 
+			| Individual Person Bool 
 			deriving Show	
+
+data Person = Person String String Gender
+            deriving Show
+
+data Gender = Male 
+			| Female 
+			| Unknown
+			deriving Show
+
+
+clientName :: Client -> String 
+clientName client = case client of
+				GovOrg name 				-> name 
+				Company name _ _ _ -> name 
+				Individual person _ 		-> 
+					case person of Person fName lName _ -> fName ++ " " ++ lName
